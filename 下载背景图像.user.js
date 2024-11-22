2f08b6b0ebfdb46726c81bcbdb49a92c781d95c2
zavch <jawayc@163.com>
2024-11-22 11:12:09 +0800

    feat(下载背景图像): 地址匹配调整

--- 下载背景图像.user.js
+++ 下载背景图像.user.js
@@ -4,7 +4,7 @@
 // @version      2024-10-17
 // @description  try to take over the world!
 // @author       You
-// @match        https://cn.bing.com/spotlight*
+// @match        https://*.bing.com/spotlight*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
